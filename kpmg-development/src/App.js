23a6c3bf3ffd5cc2b95429f1721ec32952049346
Miray Has <miray@eastpoint.co.uk>
2022-07-20 13:54:43 +0100

    New map component

--- kpmg-development/src/App.js
+++ kpmg-development/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Map from './elements/_map.jsx';
 
 function App() {
   return (
       <div id="main">
           <h1>Company Location</h1 >
+          <Map />
       </div>
   );
 }
---
+++ kpmg-development/src/elements/_map.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+
+export default function Map() {
+    return (
+        <>
+            <h1>This is a map</h1>
+        </>
+    );
+}
