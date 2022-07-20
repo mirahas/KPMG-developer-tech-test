64909cf728a543c7e8595ce02dbf20775ad530a1
Miray Has <miray@eastpoint.co.uk>
2022-07-20 16:04:26 +0100

    Add and install mapbox

--- kpmg-development/src/elements/_map.jsx
+++ kpmg-development/src/elements/_map.jsx
@@ -1,9 +1,30 @@
-import react from 'react';
+import { useEffect, useState, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
 
 export default function Map() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibWlyYXlocyIsImEiOiJja3k5eHcyeGYwMDN0Mm5yaTVhc2N5YXhsIn0.UYO7beAhrSrCTkEXJNhLMA';
+
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+
+    useEffect(() => {
+        if (map.current) return; 
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        });
+    });
+
     return (
         <>
-            <h1>This is a map</h1>
+            <div>
+                <div ref={mapContainer} className="map-container" />
+            </div>
         </>
     );
-}
+} 
