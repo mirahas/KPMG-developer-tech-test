e49c4d7abf8c7cc9a3cdcb63b72455753ab724bc
Miray Has <miray@eastpoint.co.uk>
2022-07-22 17:10:46 +0100

    data added to table

--- kpmg-development/src/elements/_table.jsx
+++ kpmg-development/src/elements/_table.jsx
@@ -10,7 +10,6 @@ export default function Table(props) {
             <table>
                 <thead>
                 <tr>
-
                     {data[0] && Object.entries(data[0]).map((key, index) => {
                         return (
                             <th key={index}>
@@ -19,8 +18,18 @@ export default function Table(props) {
                         );
                     })}
                 </tr>
-
                 </thead>
+                <tbody>
+                    <tr>
+                        {data && data.map((item) => (
+                            item && Object.entries(item).map((property, index) => (
+                                <td key={index}>
+                                    {property[1] && typeof property[1] === 'object' ? property[1][1] : property[1]}
+                                </td>
+                            ))
+                        ))}
+                    </tr>
+                </tbody>
             </table>
         </>
 
